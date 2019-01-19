1d778b08dc69a45667e74be076e0f285ac287901
Laney McGlohon <lmcglohon@gmail.com>
2019-01-18 16:00:37 -0800

    Sect2Lect9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,14 +1,40 @@
-var firstName = 'John';
-console.log(firstName);
+// var firstName = 'John';
+// console.log(firstName);
+//
+// var lastName = 'Smith';
+// var age = 28;
+//
+// var fullAge = true;
+// console.log(fullAge);
+//
+// var job;
+// console.log(job);
+//
+// job = 'teacher';
+// console.log(job);
 
-var lastName = 'Smith';
+/*********************
+* Variable mutation and type coercion
+*/
+
+// Type coercion
+
+var firstName = 'John';
 var age = 28;
 
-var fullAge = true;
-console.log(fullAge);
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
-
+var job, isMarried;
 job = 'teacher';
-console.log(job);
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);

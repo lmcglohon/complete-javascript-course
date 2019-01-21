ff04206e765e8b0611dfb26418562206dbdcfbc1
Laney McGlohon <lmcglohon@gmail.com>
2019-01-21 04:07:45 -0800

    Lecture 25

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -115,17 +115,45 @@ GOOD LUCK 😀
 // function whatDoYouDo(job, firstName){}
 
 // Function expression
-var whatDoYouDo = function(job, firstName){
-  switch(job){
-    case 'teacher':
-      return firstName + ' teaches kids how to code';
-    case 'driver':
-      return firstName + ' drives an Uber';
-    case 'designer':
-      return firstName + ' designs websites';
-    default:
-      return firstName + ' does something else';
-  }
-}
+// var whatDoYouDo = function(job, firstName){
+//   switch(job){
+//     case 'teacher':
+//       return firstName + ' teaches kids how to code';
+//     case 'driver':
+//       return firstName + ' drives an Uber';
+//     case 'designer':
+//       return firstName + ' designs websites';
+//     default:
+//       return firstName + ' does something else';
+//   }
+// }
+//
+// console.log(whatDoYouDo('driver', 'John'));
 
-console.log(whatDoYouDo('driver', 'John'));
+/******************************
+* Objects and Properties
+*/
+
+// Object literal
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// New object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
